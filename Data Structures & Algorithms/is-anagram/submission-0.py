aad450286ea4d67810eb550b0f9f7a32aea0de6d
Nicholas Vu <167171671+nickkvu@users.noreply.github.com>
2026-06-21 13:50:04 -0700

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        word1 = defaultdict(int)
+        word2 = defaultdict(int)
+
+        for letter in s :
+            word1[letter] += 1
+
+        for letter in t :
+            word2[letter] += 1
+        
+        return word1 == word2
